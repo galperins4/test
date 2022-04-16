fd4514db603fb5edc62361f7e699f64fc7fd32d4
galperins4 <galperins4@gmail.com>
2022-04-16 13:03:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ if __name__ == '__main__':
     milestone = home + "/solar-core/packages/crypto/src/networks/mainnet/milestones.json"
     f = open(milestone)
     data = json.load(f)
-    
-    print(data)
-    quit()
+    reward_dict = {int(k):v for k, v in data[2]['dynamicReward']['ranks'].items()}
        
     # get current voter information
     # do math and calculations
