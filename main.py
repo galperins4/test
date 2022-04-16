52088e4f482416ea084780652a44bee69cd29695
galperins4 <galperins4@gmail.com>
2022-04-16 12:46:53 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+
+if __name__ == '__main__':
+
+    # get block rewards
+    milestone = "~/solar-core/packages/crypto/src/networks/mainnet/milestones.json"
+    f = open(milestone)
+    data = json.load(f)
+    
+    print(data)
+    quit()
+       
+    # get current voter information
+    # do math and calculations
+    # output something
+    
+ 
